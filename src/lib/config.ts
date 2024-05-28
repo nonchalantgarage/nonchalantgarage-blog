4fc73e500515ddbbbae8c97b9baf8ab6df4d524b
Curtis Hong <hong.curtis@gmail.com>
2024-05-27 20:57:25 -0700

    footer and header

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'NonchalantGarage';
+export const description = 'A garage for the nonchalant';
+export const url = dev ? 'http://localhost:5173' : 'https://nonchalantgarage.com';
